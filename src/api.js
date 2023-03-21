c040b0f4c6da87f8a3ee94024384308e845f99f9
rajivranjanmars <rajivranjanmars@gmail.com>
2023-03-22 02:19:14 +0530

    predeploy

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export  const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '97c76ce70dmsh64a44d7c5492696p1188eejsn8f240bd7623c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY="7773a544c6bb5130a1cf85c4ba7692ec"
+
